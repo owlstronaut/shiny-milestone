45691be69bd303ff4ae9fb2886d7ca257f5fe438
Michael Smith <michael.smith@cornerstonenw.com>
2012-09-19 15:41:30 -0700

    adding sort buttons

--- js/main.js
+++ js/main.js
@@ -5,9 +5,37 @@ $(document).ready(function() {
     $("[rel=tooltip]").tooltip();
   }
 
+  setupSortBy();
+
 });
 
 function toggleIssue(evt) {
   $(evt.target).siblings('.issue-body').toggle();
+}
 
+function setupSortBy() {
+  var label_names = getLabelNames();
+  var issue_states = getIssueStates();
+  var labels = _.uniq(issue_states.concat(label_names));
+
+  var sort_by = $(document).find('.sort-by');
+  _.each(labels, function(label_data) {
+    var label = $(_.template('<span data-label="<%= label_data %>" class="active label-filter btn btn-mini"><%= label_data %></span>')({'label_data': label_data}));
+    label.click(updateVisible.bind(label));
+    sort_by.append(label);
+  });
+}
+
+function updateVisible(evt) {
+  $(evt.currentTarget).toggleClass('active');
+
+  
+}
+
+function getLabelNames() {
+  return _.map($('.issue-line .label'), function(label){return $(label).attr('data-name');});
+}
+
+function getIssueStates() {
+  return _.map($('.issue-line'), function(issue_line){return $(issue_line).attr('data-state');});
 }
--- shiny-milestone.js
+++ shiny-milestone.js
@@ -20,6 +20,7 @@ fs.readFile('.config', 'utf-8', function(err, data) {
   fs.appendFileSync('index.html', '<link rel="stylesheet" href="css/style.css">');
   fs.appendFileSync('index.html', '<h1 class="page-title">Release Notes</h1>');
   fs.appendFileSync('index.html', _.template('<h2 class="release-title"><%= milestone %></h2>')(config));
+  fs.appendFileSync('index.html', '<div class="sort-by"></div>')
 
   getRepo(config);
   fs.appendFileSync('index.html', '<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>');
@@ -52,10 +53,14 @@ function useIssues(issues, config) {
 
   _.each(issues, function(issue) {
     if (issue && issue.milestone && issue.milestone.title == config.milestone) {
-      debugger
+      if (!issue.assignee)
+        issue.assignee = {'login': 'unassigned', 'avatar_url': 'http://octodex.github.com/images/nyantocat.gif'};
+
+      if (issue.number == 488)
+        debugger
       fs.appendFileSync('index.html', _.template('<div class="issue-line" data-assignee="<%= assignee.login %>" data-assignee-avatar-url="<%= assignee.avatar_url %>" data-state="<%= state %>">')(issue));
       _.each(issue.labels, function(label) {
-        fs.appendFileSync('index.html', _.template('<span data-name="<%= name %>"" class="label" style="background-color: #<%= color %>"><%= name %></span>')(label));
+        fs.appendFileSync('index.html', _.template('<span data-name="<%= name %>" class="label" style="background-color: #<%= color %>"><%= name %></span>')(label));
       });
       fs.appendFileSync('index.html', _.template('<span class="issue-title"><%= title %> (<a href="<%= html_url %>">Issue #<%= number %></a>)</span>')(issue));
       fs.appendFileSync('index.html', '<br>');
